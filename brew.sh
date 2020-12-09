7e1b0233817bb9d7413c3fb98579b5978d3568e1
Choekyel <74311673+Choekyel@users.noreply.github.com>
2020-12-09 18:21:02 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -5,7 +5,7 @@ brew install python
 brew install tree
 
 # Install MacOS Applications
-brew cask install sublime-text
+brew install sublime-text --cask
 
 # Install Source Code Pro Font
 brew tap homebrew/cask-fonts
