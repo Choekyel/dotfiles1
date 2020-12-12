daee2e1316ad217de19e6a8a4885449573ecad93
Choekyel <74311673+Choekyel@users.noreply.github.com>
2020-12-12 20:23:19 +0100

    Sublime package fix

--- sublime.sh
+++ sublime.sh
@@ -3,12 +3,14 @@ mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Pac
 mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 
 # Install Package Control
-curl "https://packagecontrol.io/Package%20Control.sublime-package" > settings/Package\ Control.sublime-package 2> /dev/null
-cp -r settings/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package 2> /dev/null
+mkdir -p settings/package_control_installer/
+curl "https://packagecontrol.io/Package%20Control.sublime-package" > settings/package_control_installer/Package\ Control.sublime-package
+cp -r settings/package_control_installer/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package 2> /dev/null
 
 # Install Custom Sublime Text settings
 cp -r settings/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings 2> /dev/null
 cp -r settings/Anaconda.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Anaconda.sublime-settings 2> /dev/null
+cp -r settings/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings 2> /dev/null
 
 # Create Python Build Systems
 cp -r settings/Python-3.sublime-build ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/ 2> /dev/null
